550b361c7dc64468709fcc8e57935aa99f4d6402
Suhag Lapani <webdev.lapani88@gmail.com>
2020-02-28 12:06:53 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+                   <h1 slot="title">{{title}}</h1>
+                   <p>Description</p>
+               </app-quote>
+            </div>
+
+
+            <button @click="selectedComponent = 'Author'">Author</button>
+            <button @click="selectedComponent = 'New'">New</button>
+            <!--
+                destroy component when not use keep-alive
+           -->
+            <component :is="selectedComponent"></component>
+
+            <!--
+                not destroy component when use keep-alive
+            -->
+            <keep-alive>
+                <component :is="selectedComponent"></component>
+            </keep-alive>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './Components/Quote.vue'
+    import New from './Components/New.vue'
+    import Author from './Components/Author.vue'
+
+    export default {
+      data: function() {
+        return {
+          title : 'Heading 1',
+          selectedComponent: 'Author'
+        }
+      },
+      components: {
+        'appQuote': Quote,
+        Author,
+        New
+      }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/Components/Quote.vue
@@ -0,0 +1,41 @@
+<!--
+slot is use for render children
+-->
+
+<template>
+    <div>
+        <div class="title">
+            <slot name="title">
+            </slot>
+            <span style="color: #ccc"><slot name="subtitle">subtitle</slot></span>
+        </div>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+    h1 {
+        color: red;
+    }
+    .title {
+        font-size: 24px;
+        color: green;
+    }
+    .content {
+        color: cornflowerblue;
+    }
+</style>
